28cd3f973e23acb8221410e43e59abefc2de3fd7
binghao wang <takashiwangbh@gmail.com>
2025-11-12 01:56:53 +0900

    python day 1

---
+++ python_basic/day1.py
@@ -0,0 +1,13 @@
+# input hello world
+print("123")
+print("hello world")
+
+# Variables and Output
+name = "kok"
+age = 23
+print("my name is" ,name, ",my age is" ,age)
+
+# simple compute
+a = 10
+b = 20
+print("a+b= ",a+b)
